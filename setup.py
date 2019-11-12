e83d12c348ed17421f325cf3d8588e661c8d9842
TheEdoardo93 <edoardo.casiraghi.93@gmail.com>
2019-11-12 11:01:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='bert-extractive-summarizer',
-      version='0.2.0',
-      description='Extractive Text Summarization with BERT',
+      version='0.2.1',
+      description='Extractive Text Summarization with BERT (multi-linguals BERT models too!)',
       keywords = ['bert', 'pytorch', 'machine learning', 'deep learning', 'extractive summarization', 'summary'],
       long_description=open("README.md", "r", encoding='utf-8').read(),
       long_description_content_type="text/markdown",
-      url='https://github.com/dmmiller612/bert-extractive-summarizer',
-      download_url='https://github.com/dmmiller612/bert-extractive-summarizer/archive/0.2.0.tar.gz',
-      author='Derek Miller',
-      author_email='dmmiller612@gmail.com',
-      install_requires=['transformers', 'scikit-learn', 'neuralcoref', 'spacy==2.1.3'],
+      url='https://github.com/TheEdoardo93/bert-extractive-summarizer',
+      download_url='https://github.com/dmmiller612/bert-extractive-summarizer/archive/master.zip',
+      author='Edoardo Casiraghi',
+      author_email='edoardo.casiraghi.93@gmail.com',
+      install_requires=['numpy==1.16.3', 'torch==1.0.1', 'spacy==2.1.3', 'git+https://github.com/TheEdoardo93/bert-extractive-summarizer', 'Cython==0.29.10', 'tqdm==4.32.2', 'neuralcoref==4.0', 'argparse', 'scikit-learn'],
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
